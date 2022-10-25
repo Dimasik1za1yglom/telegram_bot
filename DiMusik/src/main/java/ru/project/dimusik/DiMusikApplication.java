cc0d76bb93065aba741f0aa13a3aeaef0b91301f
dkrasilnikov <dkrasilnikov@gear-games.com>
2022-10-25 15:54:30 +0300

    create project telegram bot

---
+++ DiMusik/src/main/java/ru/project/dimusik/DiMusikApplication.java
@@ -0,0 +1,13 @@
+package ru.project.dimusik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiMusikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiMusikApplication.class, args);
+    }
+
+}
---
+++ DiMusik/src/test/java/ru/project/dimusik/DiMusikApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.dimusik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiMusikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
